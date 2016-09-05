54ac96e067dad53d17ef34ee920bbf4f385ff91b
amartin125971 <amartin125971@troy.edu>
2016-09-05 04:01:38 -0500

    Map Functionality. As of now it simply takes you to the location of Troy.

--- YParking/app/src/main/java/troy/com/youparking/HomeActivity.java
+++ YParking/app/src/main/java/troy/com/youparking/HomeActivity.java
@@ -1,7 +1,9 @@
 package troy.com.youparking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void openMyMap(View view) {
+        Intent intent = new Intent(this, GeneralMap.class);
+        startActivity(intent);
     }
 
     //heyyyyy
