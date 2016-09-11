28257309ccec322ece4f437cee156510af172a5c
YouParking <tclinkscales@troy.edu>
2016-09-10 20:17:13 -0500

    Added most of the activities we will need for the project along with the navigation drawer. It only works on one screen at the moment.

---
+++ YParking/app/src/main/java/troy/com/youparking/VehiclesActivity.java
@@ -0,0 +1,12 @@
+package troy.com.youparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VehiclesActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicles);
+    }
+}
